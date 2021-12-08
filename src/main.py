a903b6a69bea5a0a3454a799c3366d1f085f57da
c1m50c <terminalityms@gmail.com>
2021-12-08 00:45:01 -0600

    Rich Formatting 💰

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from morse_code import encode, decode
+from rich.console import Console
+
 
 
 def main():
+    console: Console = Console()
+    console.print("[magenta][bold]Morse Code[/bold], Encode & Decode Morse Code.[/magenta]\n")
+    
     while True:
-        inp: str = input(">>> ")
+        inp: str = console.input("[yellow][bold]message[/bold]:$[/yellow] ")
         
-        if inp.isspace():
-            return
-        else:
-            print(encode(string=inp))
-            print(decode(string=inp))
+        if not inp: return
+        
+        console.print(f"[green bold]Encoded:[/green bold] [blue bold]{encode(string=inp)}[/blue bold]")
+        console.print(f"[red bold]Decoded:[/red bold] [blue bold]{decode(string=inp)}[/blue bold]\n")
 
 
 if __name__ == "__main__":
