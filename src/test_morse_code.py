65e87aeaeee52bd8166d03bd73dc236bdb468e07
c1m50c <terminalityms@gmail.com>
2021-12-09 16:19:49 -0600

    Assertion Fix 🔧

--- src/test_morse_code.py
+++ src/test_morse_code.py
@@ -2,8 +2,13 @@ from morse_code import encode, decode
 
 
 def test_hello_world_encoding():
-    assert encode("Hello, World!") == ".... . .-.. .-.. --- --..-- .-- --- .-. .-.. -.. -.-.--"
+    assert encode("Hello, World!") == ".... . .-.. .-.. --- --..-- .-- --- .-. .-.. -.. -.-.-- "
 
 
 def test_hello_world_decoding():
     assert decode(".... . .-.. .-.. --- --..-- .-- --- .-. .-.. -.. -.-.--") == "HELLO,WORLD!"
+
+
+if __name__ == "__main__":
+    test_hello_world_encoding()
+    test_hello_world_decoding()
