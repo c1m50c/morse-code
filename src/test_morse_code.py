f71cf92a9e9e664727a0a5bbd5466130267a3000
c1m50c <terminalityms@gmail.com>
2021-12-09 16:25:37 -0600

    More Tests 🧪

--- src/test_morse_code.py
+++ src/test_morse_code.py
@@ -9,6 +9,26 @@ def test_hello_world_decoding():
     assert decode(".... . .-.. .-.. --- --..-- .-- --- .-. .-.. -.. -.-.--") == "HELLO,WORLD!"
 
 
+def test_morse_code_endcoding():
+    assert encode("morse-code") == "-- --- .-. ... . -....- -.-. --- -.. . "
+
+
+def test_morse_code_decoding():
+    assert decode("-- --- .-. ... . -....- -.-. --- -.. .") == "MORSE-CODE"
+
+
+def test_digits_encoding():
+    assert encode("0123456789") == "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----. "
+
+
+def test_digits_decoding():
+    assert decode("----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.") == "0123456789"
+
+
 if __name__ == "__main__":
     test_hello_world_encoding()
     test_hello_world_decoding()
+    test_morse_code_endcoding()
+    test_morse_code_decoding()
+    test_digits_encoding()
+    test_digits_decoding()
